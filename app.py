d5a73e8f672fe72ffa4f39778204bd225b1ce8ae
Rema bint Amer <ug652431004@ftu.ac.th>
2025-08-07 15:39:46 +0700

    Add files via upload
    Add hello-python with Docker

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello")  # الرد بكلمة Hello
+
+if __name__ == "__main__":
+    server_address = ('', 8000)  # استمع على المنفذ 8000
+    httpd = HTTPServer(server_address, HelloHandler)
+    print("Running server on port 8000...")
+    httpd.serve_forever()
