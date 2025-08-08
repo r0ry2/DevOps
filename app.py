c8de4142ae91b649e0b0a4d5f5bab1363288f173
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-08-08 09:32:05 +0700

    GitHub

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello")  # الرد بكلمة Hello
+        self.wfile.write(b"Hello")  
 
 if __name__ == "__main__":
-    server_address = ('', 8000)  # استمع على المنفذ 8000
+    server_address = ('', 8000)
     httpd = HTTPServer(server_address, HelloHandler)
     print("Running server on port 8000...")
     httpd.serve_forever()
